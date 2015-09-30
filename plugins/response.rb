4dbf1f2a53e68a692f154aafba38fd89ee545b46
Matt Hughes <matthughes.tech@gmail.com>
2015-09-30 13:41:59 -0400

    Fix regular expression

--- plugins/response.rb
+++ plugins/response.rb
@@ -3,7 +3,7 @@ require "cinch"
 class Response
   include Cinch::Plugin
 
-  match /hello/i, :use_prefix => false, :method => :hello
+  match /^hello$/i, :use_prefix => false, :method => :hello
 
   def hello(m)
     m.reply "Hello, #{m.user.nick}!"
